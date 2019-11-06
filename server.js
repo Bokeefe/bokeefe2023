8fa2ad320a6cc4cee6ed4adbef1cbbdac3163457
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-11-06 10:35:08 -0700

    add server,profile

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+var fs = require('fs');
+
+app.use(express.static(__dirname + '/dist/'));
+
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('app listening on port 3000!');
+});
